dd75cd1e110e1a059317a63761e1c6cfeaf65cf6
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-08-17 11:04:01 +0530

    new

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 win = Tk()
 win.title("Sachith Priyamantha")
 win.config(bg = "cyan")
-win.geometry("600x500")
+win.geometry("600x570")
 
 
 name_label = Label(win,text="Sachi Weather App",
@@ -33,5 +33,39 @@ done_button.place(y=190,height=50,width=100,x=250)
 w_label = Label(win,text="Weather Climate",
                    font=("monospace",20))
 w_label.place(y=260,height=50,width=210,x=25)
+w_label1 = Label(win,text="",
+                   font=("monospace",20))
+w_label1.place(y=260,height=50,width=210,x=250)
+
+
+wb_label = Label(win,text="Weather Description",
+                   font=("monospace",16))
+wb_label.place(y=330,height=50,width=210,x=25)
+wb_label1 = Label(win,text="",
+                   font=("monospace",16))
+wb_label1.place(y=330,height=50,width=210,x=250)
+
+
+temp_label = Label(win,text="Temperature",
+                   font=("monospace",20))
+temp_label.place(y=400,height=50,width=210,x=25)
+temp_label1 = Label(win,text="",
+                   font=("monospace",20))
+temp_label1.place(y=400,height=50,width=210,x=250)
+
+
+per_label = Label(win,text="Pressure",
+                   font=("monospace",20))
+per_label.place(y=470,height=50,width=210,x=25)
+per_label1 = Label(win,text="",
+                   font=("monospace",20))
+per_label1.place(y=470,height=50,width=210,x=250)
+
+
+
+
+
+
+
 
 win.mainloop()
